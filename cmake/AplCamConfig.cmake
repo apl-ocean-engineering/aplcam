93291d4f8b3a5de042749862f36799005a58e6f1
Aaron Marburg <amarburg@apl.washington.edu>
2015-10-07 14:53:02 -0700

    Moved SonarPose to AplCam.

---
+++ cmake/AplCamConfig.cmake
@@ -0,0 +1,41 @@
+
+
+if (NOT aplcam_SOURCE_DIR)
+	message( FATAL_ERROR "Hmm, aplcam_SOURCE_DIR not set" )
+endif ()
+
+## AplCam has external dependencies
+list(APPEND CMAKE_MODULE_PATH "${aplcam_SOURCE_DIR}/cmake")
+find_package( GSL REQUIRED )
+find_package( OpenCV REQUIRED )
+find_package( Ceres REQUIRED )
+find_package( Boost REQUIRED COMPONENTS thread system filesystem )
+set( Boost_USE_MULTITHREADED ON )
+if( USE_APRILTAGS )
+  find_package( Apriltags REQUIRED )
+	set( apriltags_LIBS libapriltags )
+endif()
+
+ExternalProject_Get_Property( aplcam SOURCE_DIR BINARY_DIR )
+add_library( libaplcam STATIC IMPORTED )
+set_property( TARGET libaplcam PROPERTY IMPORTED_LOCATION ${BINARY_DIR}/lib/libaplcam.a )
+
+set( aplcam_INCLUDE_DIRS
+     ${SOURCE_DIR}/include
+     ${apriltags_INCLUDE_DIRS}
+     ${OpenCV_INCLUDE_DIRS}
+		 ${EIGEN_INCLUDE_DIR}
+		 ${GSL_INCLUDE_DIRS}
+		${CERES_INCLUDE_DIRS} )
+
+set( aplcam_LIBS
+     libaplcam
+     ${apriltags_LIBS}
+     cryptopp
+     ${OpenCV_LIBS}
+     ${GSL_LIBRARIES}
+     ${Boost_LIBRARIES}
+ 		 ${CERES_LIBRARIES}
+		 gtest )
+
+find_package_handle_standard_args( aplcam DEFAULT_MSG aplcam_INCLUDE_DIRS aplcam_LIBS )
